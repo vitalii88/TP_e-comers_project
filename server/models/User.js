196a027a6909e427129086e53487c1dc5ea9ed88
Vitalii Bairak <vitalii_b88@yahoo.com>
2022-10-17 22:03:04 -0400

    feat(server): created User Schema
    "re #1"

---
+++ server/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide name'],
+    minlength: 3,
+    maxlength: 20,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please, provide email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please, provide password'],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ['admin', 'user'],
+    default: 'user'
+  },
+});
+
+export default mongoose.model('User', UserSchema);
